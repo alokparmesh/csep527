a43786dce2ae0893be7396d37266251bcbfb8d7c
alokparmesh <alokparmesh@gmail.com>
2016-05-01 23:52:30 -0700

    ParameterEstimation
    ParameterEstimation

---
+++ ParameterEstimation/EMAlgorithm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterEstimation
+{
+    public class EMAlgorithm
+    {
+        private const double sd = 1.0;
+        private readonly double tau;
+        private readonly int k;
+        private readonly int n;
+        private readonly double[] numbers;
+
+        private double[] mu;
+        private double[,] z;
+
+
+        public EMAlgorithm(List<double> numbers, int numOfMixtures)
+        {
+            this.k = numOfMixtures;
+            this.numbers = numbers.ToArray();
+            this.n = this.numbers.Length;
+            this.tau = 1.0 / k;
+
+            this.mu = new double[this.k];
+            this.z = new double[n, k];
+        }
+
+        private void InitMu()
+        {
+            /*double max = this.numbers.Max();
+            double min = this.numbers.Min();
+
+            double step = (max - min) / (k - 1);
+
+            this.mu[0] = min;
+            for(int i = 1; i < k; i++)
+            {
+                this.mu[i] = min + i * step;
+            }*/
+
+            this.mu[0] = -20;
+            this.mu[1] = 6;
+            //this.mu[2] = 55;
+        }
+
+        public void Calculate()
+        {
+            this.InitMu();
+
+            for (int i = 0; i < 5; i++)
+            {
+                EStep();
+                MStep();
+            }          
+        }
+
+        private void EStep()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                double number = this.numbers[i];
+
+                double sum = 0.0;
+                for (int j = 0; j < k; j++)
+                {
+                    z[i, j] = GuassianDensity(number, this.mu[j], EMAlgorithm.sd);
+                    sum += z[i, j];
+                }
+
+                if (sum > 0)
+                {
+                    for (int j = 0; j < k; j++)
+                    {
+                        z[i, j] = z[i, j] / sum;
+                    }
+                }
+            }
+        }
+
+        private void MStep()
+        {
+            for (int j = 0; j < k; j++)
+            {
+                double exi = 0.0;
+                double e = 0.0;
+
+                for (int i = 0; i < n; i++)
+                {
+                    exi += this.z[i, j] * this.numbers[i];
+                    e += this.z[i, j];
+                }
+
+                if(e > 0)
+                {
+                    this.mu[j] = exi / e;
+                }
+
+                Console.Write(this.mu[j]);
+                Console.Write("\t");
+            }
+            Console.WriteLine();
+        }
+
+        private static double GuassianDensity(double number, double average, double sd)
+        {
+            double d = (number - average) / sd;
+            double exp = Math.Exp(-1.0 * (d * d) / 2);
+            return exp / (sd * Math.Sqrt(2 * Math.PI));
+        }
+    }
+}
---
+++ ParameterEstimation/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParameterEstimation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                throw new ArgumentException("Only one parameter expected");
+            }
+
+            string fileName = args[0];
+
+            if (!File.Exists(fileName))
+            {
+                throw new ArgumentException(string.Format("File not found {0}", fileName));
+            }
+
+            List<double> numbers = GetNuberList(fileName);
+
+            PrintEstimation(numbers, 2);
+        }
+
+        private static void PrintEstimation(List<double> numbers, int numMixtures)
+        {
+            if (numMixtures == 1)
+            {
+                double average = numbers.Average();
+                double sumOfSquares = numbers.Select(val => (val - average) * (val - average)).Sum();
+                double sd = Math.Sqrt(sumOfSquares / (numbers.Count - 1));
+                Console.WriteLine(average.ToString("#.00000"));
+                Console.WriteLine(sd.ToString("#.00000"));
+
+                double product = GetLikelihood(numbers, average, sd);
+
+                Console.WriteLine(Math.Log(product).ToString("#.00000"));
+                Console.WriteLine(2 *Math.Log(product) - 1 * Math.Log(numbers.Count));
+            }
+            else
+            {
+                EMAlgorithm algo = new EMAlgorithm(numbers, numMixtures);
+                algo.Calculate();
+            }
+        }
+
+        private static double GetLikelihood(List<double> numbers, double average, double sd)
+        {
+            double product = 1.0;
+
+            foreach (double number in numbers)
+            {
+                double Likelihood = GuassianLikelihood(number, average, sd);
+                product *= Likelihood;
+                
+                /*
+                double Likelihood1 = GuassianLikelihood(number, 21, 1) / 3;
+                double Likelihood2 = GuassianLikelihood(number, 46, 1) / 3;
+                double Likelihood3 = GuassianLikelihood(number, 55, 1) / 3;
+                product *= (Likelihood1+Likelihood2+Likelihood3);
+                */
+            }
+
+            return product;
+        }
+
+        private static double GuassianLikelihood(double number, double average, double sd)
+        {
+            double d = (number - average) / sd;
+            double exp = Math.Exp(-1.0 * (d * d) / 2);
+            return exp / (sd * Math.Sqrt(2 * Math.PI));
+        }
+
+        private static List<double> GetNuberList(string fileName)
+        {
+            List<double> numbers = new List<double>();
+
+            using (var sr = new StreamReader(fileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string s = sr.ReadLine();
+                    if (s == null || string.IsNullOrWhiteSpace(s))
+                    {
+                        // empty or comment;
+                        continue;
+                    }
+
+                    string[] numberStringArray = s.Trim().Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (string numberString in numberStringArray)
+                    {
+                        double number;
+                        if (double.TryParse(numberString, out number))
+                        {
+                            numbers.Add(number);
+                        }
+                    }
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
