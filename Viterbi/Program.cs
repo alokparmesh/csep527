65db0dc931b1078ce0b35a197bdf593e7e055ab1
alokparmesh <alokparmesh@gmail.com>
2016-05-15 23:57:43 -0700

    Add Viterbi
    Add Viterbi

---
+++ Viterbi/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viterbi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ViterbiAlgorithm algo = new ViterbiAlgorithm(2);
+            char loaded = 'L';
+            char fair = 'F';
+
+            algo.HiddenStates[0] = loaded;
+            algo.HiddenStates[1] = fair;
+
+            algo.InitialTransitionProbabilities[0] = 0.52;
+            algo.InitialTransitionProbabilities[1] = 0.48;
+
+            algo.TransitionProbabilities[0, 0] = 0.60;
+            algo.TransitionProbabilities[0, 1] = 0.40;
+            algo.TransitionProbabilities[1, 0] = 0.17;
+            algo.TransitionProbabilities[1, 1] = 0.83;
+
+            Dictionary<char, double> loadedProbabilities = new Dictionary<char, double>();
+            loadedProbabilities.Add('1', 0.1);
+            loadedProbabilities.Add('2', 0.1);
+            loadedProbabilities.Add('3', 0.1);
+            loadedProbabilities.Add('4', 0.1);
+            loadedProbabilities.Add('5', 0.1);
+            loadedProbabilities.Add('6', 0.5);
+            algo.EmissionProbabilities[0] = loadedProbabilities;
+
+            Dictionary<char, double> fairProbabilities = new Dictionary<char, double>();
+            fairProbabilities.Add('1', 1.0/6.0);
+            fairProbabilities.Add('2', 1.0/6.0);
+            fairProbabilities.Add('3', 1.0/6.0);
+            fairProbabilities.Add('4', 1.0/6.0);
+            fairProbabilities.Add('5', 1.0/6.0);
+            fairProbabilities.Add('6', 1.0/6.0);
+            algo.EmissionProbabilities[1] = fairProbabilities;
+
+            Console.WriteLine(algo.TraceBack("316664"));
+        }
+    }
+}
---
+++ Viterbi/ViterbiAlgorithm.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viterbi
+{
+    public class ViterbiAlgorithm
+    {
+        private readonly int numHiddenStates;
+        public Dictionary<char,double>[] EmissionProbabilities { get; set; }
+
+        public double[,] TransitionProbabilities { get; set; }
+
+        public double[] InitialTransitionProbabilities { get; set; }
+
+        public char[] HiddenStates { get; set; }
+
+        public ViterbiAlgorithm(int numHiddenStates)
+        {
+            this.numHiddenStates = numHiddenStates;
+            this.HiddenStates = new char[numHiddenStates];
+            this.TransitionProbabilities = new double[numHiddenStates, numHiddenStates];
+            this.InitialTransitionProbabilities = new double[numHiddenStates];
+            this.EmissionProbabilities = new Dictionary<char, double>[numHiddenStates];
+        }
+
+        public string TraceBack(string sequence)
+        {
+            StringBuilder[] previousTraceBackSequences = new StringBuilder[this.numHiddenStates];
+            double[] previousSequenceProbabilities = new double[this.numHiddenStates];
+
+            StringBuilder[] currentTraceBackSequences;
+            double[] currentSequenceProbabilities;
+
+            for (int i = 0; i < this.numHiddenStates; i++)
+            {
+                previousTraceBackSequences[i] = new StringBuilder();
+                previousTraceBackSequences[i].Append(this.HiddenStates[i]);
+                previousSequenceProbabilities[i] = this.InitialTransitionProbabilities[i] * this.EmissionProbabilities[i][sequence[0]];
+            }
+
+            int sequenceLength = sequence.Length;
+
+            for (int i = 1; i < sequenceLength; i++)
+            {
+                currentSequenceProbabilities = new double[this.numHiddenStates];
+                currentTraceBackSequences = new StringBuilder[this.numHiddenStates];
+                char currentEmission = sequence[i];
+
+                for (int k = 0; k < this.numHiddenStates; k++)
+                {
+                    double maxSequenceProbability = 0.0;
+                    for (int j = 0; k < this.numHiddenStates; j++)
+                    {
+                        double sequenceProbability = previousSequenceProbabilities[k] * this.TransitionProbabilities[k,j] * this.EmissionProbabilities[j][currentEmission];
+
+                        if(sequenceProbability > maxSequenceProbability)
+                        {
+                            maxSequenceProbability = sequenceProbability;
+                        }
+
+                    }
+
+                    currentSequenceProbabilities[k] = maxSequenceProbability;
+                }
+
+                previousSequenceProbabilities = currentSequenceProbabilities;
+                previousTraceBackSequences = currentTraceBackSequences;
+            }
+        }
+    }
+}
